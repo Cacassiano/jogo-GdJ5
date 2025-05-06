3014dc780f89c397fd4d90098b3aa5ae11440635
Cacassiano <saurinho.64.ss@gmail.com>
2025-05-06 14:37:16 -0300

    Simpllificando  a mov jogador

---
+++ Player.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+public partial class Player : Area2D
+{
+	[Export]
+	public int Speed = 400;
+	[Export]
+	public float RunMulti = 2f;
+	public override void _Process(double delta)
+	{
+		var _velocity = new Vector2();
+
+		_velocity = Input.GetVector("MovEsquerda","MovDireita", "MovCima", "MovBaixo");
+
+		int mySpeed = Input.IsActionPressed("MovCorrer") ? (int)(Speed*RunMulti) : Speed;
+		_velocity = _velocity.Normalized() * mySpeed;
+
+		Position += _velocity * (float)delta;
+		
+	}
+}
